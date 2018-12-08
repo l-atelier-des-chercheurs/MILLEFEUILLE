24665f5e6e01949c133b619526062eb2efc79f17
Louis Eveillard <barlou15+github@gmail.com>
2018-12-08 15:18:20 +0100

    fixed animation when layer opacity is low

--- public/src/vue/components/Calques.vue
+++ public/src/vue/components/Calques.vue
@@ -260,10 +260,13 @@ export default {
         opts['opacity'] = opacity;
       }
 
+      if(!this.slugLayersShown.includes(slugLayerName)) {
+        opts['opacity'] = 0;
+      }
+
       if(this.$root.settings.mode_perspective) {
         const move_layer_up_based_on_index = (index * this.$root.settings.perspective_stretch) - (this.slugLayersShown.length * this.$root.settings.perspective_stretch)/2;
         const stretch_factor = this.$root.settings.sidebar.view === 'Layers' ? move_layer_up_based_on_index : this.$root.settings.perspective_stretch;
-
         opts['transform'] = `rotateX(45deg) rotate(-45deg) scale(1) translate3d(0px, 0px, ${stretch_factor}px)`;
       }
 
